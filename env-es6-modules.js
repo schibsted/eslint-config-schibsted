4b6efbeac9fba94f1da18c3df58e422bfefa2e45
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2015-11-11 10:34:18 +0100

    Fix linting errors in env-es6-modules.js

--- env-es6-modules.js
+++ env-es6-modules.js
@@ -1,12 +1,12 @@
 /*
     Load ES6 settings as well as the ES6 modeule settings, since the modules are of no use otherwise.
 */
-const _ = require("lodash");
-const envEs6 = require("./env-es6");
+const _ = require('lodash');
+const envEs6 = require('./env-es6');
 const defaults = {
-  "ecmaFeatures": {
-    "modules": true
-  }
+    'ecmaFeatures': {
+        'modules': true
+    }
 };
 
 module.exports = _.merge({}, envEs6, defaults);
